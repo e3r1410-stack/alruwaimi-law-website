685353bbe88df54a6a049494920165d72e3df527
Ibtisam Alruwaimi <a.aldaghfaq@gmail.com>
2026-03-24 00:21:05 +0300

    Worker: simplify charset fix - set UTF-8 on all HTML responses

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     const response = await env.ASSETS.fetch(request);
     const contentType = response.headers.get('Content-Type');
-    if (contentType && contentType.includes('text/html') && !contentType.includes('charset')) {
+    if (contentType && contentType.includes('text/html')) {
       const newHeaders = new Headers(response.headers);
       newHeaders.set('Content-Type', 'text/html; charset=UTF-8');
       return new Response(response.body, {
