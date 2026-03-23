258f89e3fc91b1447ec2ac3e8730622095c9d179
Ibtisam Alruwaimi <a.aldaghfaq@gmail.com>
2026-03-24 00:15:20 +0300

    Fix Arabic charset: add worker.js to set Content-Type UTF-8 header

---
+++ worker.js
@@ -0,0 +1,16 @@
+export default {
+  async fetch(request, env) {
+    const response = await env.ASSETS.fetch(request);
+    const contentType = response.headers.get('Content-Type');
+    if (contentType && contentType.includes('text/html') && !contentType.includes('charset')) {
+      const newHeaders = new Headers(response.headers);
+      newHeaders.set('Content-Type', 'text/html; charset=UTF-8');
+      return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: newHeaders,
+      });
+    }
+    return response;
+  }
+};
